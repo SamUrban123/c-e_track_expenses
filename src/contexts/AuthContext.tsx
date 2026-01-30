383c1baedaed45c249898f43b3f98f0ff2dc6ee7
Samuel Urban <surban@americanbatterysolutions.com>
2026-01-30 15:49:29 -0500

    Fix_unused_import_to_resolve_build_failure

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, useEffect } from 'react';
+import React, { createContext, useContext, useState } from 'react';
 import { googleLogout, useGoogleLogin, type TokenResponse } from '@react-oauth/google';
 import { config } from '../config/appConfig';
 
